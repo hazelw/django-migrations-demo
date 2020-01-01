1cd25a488802670f0a0239e8ecaa9c852e43d5e3
Hazel Wright <hazel.wright@zego.com>
2020-01-01 20:43:54 +0000

    feat: fix things up with first example

--- example-1/blog/posts/models.py
+++ example-1/blog/posts/models.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=10000)
     created_at = models.DateTimeField(default=datetime.now)
@@ -11,9 +12,12 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User)
-    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
     text = models.CharField(max_length=5000)
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
-    comment_type = models.ChoiceField(['question', 'comment'])
+    comment_type = models.CharField(
+        choices=[('question', 'question'), ('comment', 'comment')],
+        max_length=10
+    )
